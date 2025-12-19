3c7ac068d9a03eeb9ed85abaf0cdefa8a61970e8
Avishka Piumal <piumal302@gmail.com>
2025-12-19 22:30:59 +0530

    refactor Screen.kt to update navigation structure and add Support and About screens

--- app/src/main/java/com/piumal/filedownloadmanager/ui/navigation/Screen.kt
+++ app/src/main/java/com/piumal/filedownloadmanager/ui/navigation/Screen.kt
@@ -1,11 +1,6 @@
 package com.piumal.filedownloadmanager.ui.navigation
-
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Settings
-
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
+
 import com.piumal.filedownloadmanager.R
 
 sealed class Screen(
@@ -14,14 +9,12 @@ sealed class Screen(
     val iconRes : Int? = null,
     val icon: ImageVector? = null
 ) {
-    //object Home : Screen("home", "Home", Icons.Default.Home)
     object Downloads : Screen("downloads", "Downloads", iconRes= R.drawable.download_24px)
     object Browser : Screen("browser", "Browser", iconRes= R.drawable.web_asset_24px)
     object Settings : Screen("settings", "Settings", iconRes= R.drawable.settings_24px)
+    object Support : Screen("help", "Help & Support", iconRes= R.drawable.help_center_24px)
 
-    object Help : Screen("settings", "Settings", iconRes= R.drawable.help_center_24px)
-
-    object info : Screen("settings", "Settings", iconRes= R.drawable.info_24px)
+    object About : Screen("info", "About", iconRes= R.drawable.info_24px)
 
 
 
@@ -32,6 +25,6 @@ val bottomNavItems = listOf(
     Screen.Downloads,
     Screen.Browser,
     Screen.Settings,
-    Screen.Help,
-    Screen.info
+    Screen.Support,
+    Screen.About
 )
